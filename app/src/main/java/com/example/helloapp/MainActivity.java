3649845b60620a4693d846bd8ff5ce61970e6dc9
Zolomed <sasha.zolotarev10@gmail.com>
2023-02-16 21:07:49 +0300

    Added logs

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -3,36 +3,47 @@ package com.example.helloapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyApp";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        Log.e(TAG,"OnCreate");
         setContentView(R.layout.activity_main);
     }
     protected void onStart(){
         super.onStart();
         Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+        Log.w(TAG,"OnStart");
     }
     protected void onResume(){
         super.onResume();
         Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"OnResume");
     }
     protected void onPause(){
         super.onPause();
         Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"OnPause");
     }
     protected void onStop(){
         super.onStop();
         Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        Log.v(TAG,"OnStop");
     }
     protected void onRestart(){
         super.onRestart();
         Toast.makeText(this, "OnRestart", Toast.LENGTH_SHORT).show();
+        Log.v(TAG,"OnRestart");
     }
     protected void onDestroy(){
         super.onDestroy();
         Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        Log.v(TAG,"OnDestroy");
     }
 }
