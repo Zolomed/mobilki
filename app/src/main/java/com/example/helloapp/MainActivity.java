df9e3f53498d53eadc0cc4bdd46a76ca4a246561
Zolomed <sasha.zolotarev10@gmail.com>
2023-02-16 21:00:24 +0300

    Added toasts

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.helloapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_main);
+    }
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+    }
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+    }
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+    }
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+    }
+    protected void onRestart(){
+        super.onRestart();
+        Toast.makeText(this, "OnRestart", Toast.LENGTH_SHORT).show();
+    }
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
     }
 }
